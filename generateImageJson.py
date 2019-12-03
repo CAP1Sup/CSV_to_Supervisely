8ac56a14f67b6c990f4bf703f92abf6e50821850
Christian Piper <xmanpiper@gmail.com>
2019-12-02 19:23:38 -0500

    Added examples and started work on independent file creators

---
+++ generateImageJson.py
@@ -0,0 +1,15 @@
+# Christian Piper
+# 12/2/19
+# This file contains the functions and code necessary to build .json files for each of the images based on the .csv or .xml files
+
+import os
+
+fileContents = ""
+
+
+def generateJson():
+    pass
+
+def addString(addition):
+    global fileContents
+    fileContents = fileContents + str(addition)
